723b34905d0241bbd5345624b0936f778b07f9a7
jonapsjaeger <45205299+jonapsjaeger@users.noreply.github.com>
2018-11-20 17:02:47 +0100

    Add files via upload

---
+++ app/mock-heroes.ts
@@ -0,0 +1,81 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+  {
+    id: 11,
+    name: "Iron Man",
+    ability: "Money",
+    firstMovie: "Iron Man",
+    firstMovieYear: 2008
+  },
+  {
+    id: 12,
+    name: "Hulk",
+    ability: "superhuman strength",
+    firstMovie: "The Incredible Hulk",
+    firstMovieYear: 2008
+  },
+  {
+    id: 13,
+    name: "Thor",
+    ability: "God Of Thunder",
+    firstMovie: "Thor",
+    firstMovieYear: 2011
+  },
+  {
+    id: 14,
+    name: "Captain America",
+    ability: "Biomedical Enhancement",
+    firstMovie: "Captain America: The First Avenger",
+    firstMovieYear: 2011
+  },
+  {
+    id: 15,
+    name: "Starlord",
+    ability: "Ego and Intelligence",
+    firstMovie: "Guardians of the Galaxy",
+    firstMovieYear: 2014
+  },
+  {
+    id: 16,
+    name: "Groot",
+    ability: "Tree growth",
+    firstMovie: "Guardians of the Galaxy",
+    firstMovieYear: 2014
+  },
+  {
+    id: 17,
+    name: "Gamora",
+    ability: "Green Assassin",
+    firstMovie: "Guardians of the Galaxy",
+    firstMovieYear: 2014
+  },
+  {
+    id: 18,
+    name: "Rocket",
+    ability: "Raccoon",
+    firstMovie: "Guardians of the Galaxy",
+    firstMovieYear: 2014
+  },
+  {
+    id: 19,
+    name: "Ant-Man",
+    ability: "Shapechanger",
+    firstMovie: "Ant-Man",
+    firstMovieYear: 2015
+  },
+  {
+    id: 20,
+    name: "Dr. Strange",
+    ability: "Time",
+    firstMovie: "Doctor Strange",
+    firstMovieYear: 2016
+  },
+  {
+    id: 21,
+    name: "Black Panther",
+    ability: "Agil and Strength",
+    firstMovie: "Black Panther",
+    firstMovieYear: 2018
+  }
+];
